cc31e938216604766031f5702179568f8e6a24eb
taitaisuke-art <56817737+taitaisuke-art@users.noreply.github.com>
2019-11-12 16:28:57 +0900

    setting finish

--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.content                @message.content
 json.user_name              @message.user.name
-json.created_at             @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.created_at             @message.created_at.strftime("%Y/%m/%d(#{%w(Sun Mon Tue Wed Thu Fri Sat)[Time.now.wday]}) %H:%M:%S")
 json.image                  @message.image.url
 json.id                     @message.id
--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,15 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    config.action_view.automatically_disable_submit_tag = false
+    #config.action_view.automatically_disable_submit_tag = false
     config.i18n.default_locale = :ja
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.time_zone = 'Tokyo'
+      config.active_record.default_timezone = :local
     end
   end
 end
