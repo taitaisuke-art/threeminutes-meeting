e98abb89e0cc2fbcb037c33308b3ebbafbe609ce
taitaisuke-art <56817737+taitaisuke-art@users.noreply.github.com>
2020-01-16 15:04:19 +0900

    henkou

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1' #例 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'taitaisukeart'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/taitaisukeart'
+  config.fog_directory  = 'bluemeeting'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bluemeeting'
 end
